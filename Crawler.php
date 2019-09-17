e26e332ba62c6e9c8a2c918cdb77a6da9f1ae66b
Marcin Dyszkiewicz <30576974+MarcinDyszkiewicz@users.noreply.github.com>
2019-09-17 09:29:05 +0200

    Update Crawler.php

--- Crawler.php
+++ Crawler.php
@@ -45,7 +45,7 @@ class Crawler implements \Countable, \IteratorAggregate
     /**
      * @var \DOMNode[]
      */
-    private $nodes = [];
+    public $nodes = [];
 
     /**
      * Whether the Crawler contains HTML or XML content (used when converting CSS to XPath).
